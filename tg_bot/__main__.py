9138acf3362100c0ea8a0c8930abe10c5b0e8b9a
Roj <49933115+rojserbest@users.noreply.github.com>
2020-10-23 17:32:57 +0300

    Add files via upload

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -140,9 +140,9 @@ def start(bot: Bot, update: Update, args: List[str]):
 
         else:
             first_name = update.effective_user.first_name
-            update.effective_message.reply_text(
-                PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
-                parse_mode=ParseMode.MARKDOWN)
+            update.effective_message.reply_photo(
+                photo=open("photo.jpg", "rb"), caption=PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
+                parse_mode=ParseMode.MARKDOWN, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("زیادم بکە بۆ گروپەکەت", url='http://t.me/{}?startgroup=botstart'.format(bot.username))]]))
     else:
         update.effective_message.reply_text("Yo, whadup?")
 
