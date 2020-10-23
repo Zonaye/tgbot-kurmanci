61839d68c7a1227795b2bba10cc2a0d8154595bc
Roj <49933115+rojserbest@users.noreply.github.com>
2020-10-23 11:09:22 +0300

    Update msg_deleting.py

--- tg_bot/modules/msg_deleting.py
+++ tg_bot/modules/msg_deleting.py
@@ -50,7 +50,7 @@ def purge(bot: Bot, update: Update, args: List[str]) -> str:
                 elif err.message != "Message to delete not found":
                     LOGGER.exception("Error while purging chat messages.")
 
-            bot.send_message(chat.id, "پاککردنەوە تەواوبوو.")
+            bot.send_message(chat.id, "خاوێنکردنەوە تەواوبوو.")
             return "<b>{}:</b>" \
                    "\n#PURGE" \
                    "\n<b>بەڕێوەبەر:</b> {}" \
