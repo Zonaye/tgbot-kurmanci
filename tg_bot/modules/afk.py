2ece084f7535891d87007c1dd6ade0f30ba2d4c7
Roj <49933115+rojserbest@users.noreply.github.com>
2020-10-23 14:35:03 +0300

    Add files via upload

--- tg_bot/modules/afk.py
+++ tg_bot/modules/afk.py
@@ -22,7 +22,7 @@ def afk(bot: Bot, update: Update):
         reason = ""
 
     sql.set_afk(update.effective_user.id, reason)
-    update.effective_message.reply_text("{} is now AFK!".format(update.effective_user.first_name))
+    update.effective_message.reply_text("بەکارهێنەر {} لەسەرهێڵ نەما!".format(update.effective_user.first_name))
 
 
 @run_async
@@ -34,7 +34,7 @@ def no_longer_afk(bot: Bot, update: Update):
 
     res = sql.rm_afk(user.id)
     if res:
-        update.effective_message.reply_text("{} is no longer AFK!".format(update.effective_user.first_name))
+        update.effective_message.reply_text("بەکارهێنەر {} گەڕایەوە سەرهێڵ!".format(update.effective_user.first_name))
 
 
 @run_async
@@ -62,9 +62,9 @@ def reply_afk(bot: Bot, update: Update):
                 valid, reason = sql.check_afk_status(user_id)
                 if valid:
                     if not reason:
-                        res = "{} is AFK!".format(fst_name)
+                        res = "بەکارهێنەر {} لەسەرهێڵ نییە!".format(fst_name)
                     else:
-                        res = "{} is AFK! says its because of:\n{}".format(fst_name, reason)
+                        res = "بەکارهێنەر {} لەسەرهێڵ نییە! لەبەر ئەم هۆکارە:\n{}".format(fst_name, reason)
                     message.reply_text(res)
 
 
