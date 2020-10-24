5c673ddf019c59b653548ed0d54a0562de98f5b0
Roj <49933115+rojserbest@users.noreply.github.com>
2020-10-24 12:00:26 +0300

    Update channel_join.py

--- tg_bot/modules/channel_join.py
+++ tg_bot/modules/channel_join.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from telegram import Message, Update, Bot, User
+from telegram import Message, Update, Bot, User, InlineKeyboardMarkup, KeyboardButton
 from telegram import MessageEntity
 from telegram.ext import Filters, CommandHandler, MessageHandler, run_async
 
