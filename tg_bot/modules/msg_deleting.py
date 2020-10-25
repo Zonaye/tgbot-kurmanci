18265581581a371a420a48c9e635091ab3f9bf05
Roj <49933115+rojserbest@users.noreply.github.com>
2020-10-25 14:05:41 +0300

    Update msg_deleting.py

--- tg_bot/modules/msg_deleting.py
+++ tg_bot/modules/msg_deleting.py
@@ -55,8 +55,6 @@ def purge(bot: Bot, update: Update, args: List[str]) -> str:
                    "\nPurged <code>{}</code> messages.".format(html.escape(chat.title),
                                                                mention_html(user.id, user.first_name),
                                                                delete_to - message_id)
-                                                               mention_html(user.id, user.first_name),
-                                                               delete_to - message_id))
 
     else:
         msg.reply_text("Bersiva peyamekê bide.")
