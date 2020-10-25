3eff0fda52a7e59ce884dde814348e7a82b0bfb6
Roj <49933115+rojserbest@users.noreply.github.com>
2020-10-25 11:52:07 +0300

    Update msg_deleting.py

--- tg_bot/modules/msg_deleting.py
+++ tg_bot/modules/msg_deleting.py
@@ -82,7 +82,7 @@ def del_message(bot: Bot, update: Update) -> str:
                    "\nMessage deleted.".format(html.escape(chat.title),
                                                mention_html(user.id, user.first_name))
     else:
-        update.effective_message.reply_text("دەتەوێت چی بسڕیتەوە؟")
+        update.effective_message.reply_tex("Dixwazî çi jê bibî?")
 
     return ""
 
