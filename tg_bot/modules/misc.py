cd90fce73aa3183a7bbc08a4f0ea8e3c2f911027
Roj <49933115+rojserbest@users.noreply.github.com>
2020-10-26 13:06:49 +0300

    Add files via upload

---
+++ tg_bot/modules/misc.py
@@ -0,0 +1,195 @@
+import html
+import json
+import random
+from datetime import datetime
+from typing import Optional, List
+
+import requests
+from telegram import Message, Chat, Update, Bot, MessageEntity
+from telegram import ParseMode
+from telegram.ext import CommandHandler, run_async, Filters
+from telegram.utils.helpers import escape_markdown, mention_html
+
+from tg_bot import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, WHITELIST_USERS, BAN_STICKER
+from tg_bot.__main__ import GDPR
+from tg_bot.__main__ import STATS, USER_INFO
+from tg_bot.modules.disable import DisableAbleCommandHandler
+from tg_bot.modules.helper_funcs.extraction import extract_user
+from tg_bot.modules.helper_funcs.filters import CustomFilters
+
+
+@run_async
+def get_bot_ip(bot: Bot, update: Update):
+    """ Sends the bot's IP address, so as to be able to ssh in if necessary.
+        OWNER ONLY.
+    """
+    res = requests.get("http://ipinfo.io/ip")
+    update.message.reply_text(res.text)
+
+
+@run_async
+def get_id(bot: Bot, update: Update, args: List[str]):
+    user_id = extract_user(update.effective_message, args)
+    if user_id:
+        if update.effective_message.reply_to_message and update.effective_message.reply_to_message.forward_from:
+            user1 = update.effective_message.reply_to_message.from_user
+            user2 = update.effective_message.reply_to_message.forward_from
+            update.effective_message.reply_text(
+                "Wî kesê ev şandiye, {}, IDya wî ev e: `{}`.\n\nWî kesê veguhestiye, {}, IDya wî ev e: `{}`.").format(
+                    escape_markdown(user2.first_name),
+                    user2.id,
+                    escape_markdown(user1.first_name),
+                    user1.id),
+                parse_mode=ParseMode.MARKDOWN)
+        else:
+            user = bot.get_chat(user_id)
+            update.effective_message.reply_text("IDya {} ev e: `{}`.".format(escape_markdown(user.first_name), user.id),
+                                                parse_mode=ParseMode.MARKDOWN)
+    else:
+        chat = update.effective_chat  # type: Optional[Chat]
+        if chat.type == "private":
+            update.effective_message.reply_text("IDya te: `{}`.".format(chat.id),
+                                                parse_mode=ParseMode.MARKDOWN)
+
+        else:
+            update.effective_message.reply_text("IDya vê çatê: `{}`.".format(chat.id),
+                                                parse_mode=ParseMode.MARKDOWN)
+
+
+@run_async
+def info(bot: Bot, update: Update, args: List[str]):
+    msg = update.effective_message  # type: Optional[Message]
+    user_id = extract_user(update.effective_message, args)
+
+    if user_id:
+        user = bot.get_chat(user_id)
+
+    elif not msg.reply_to_message and not args:
+        user = msg.from_user
+
+    elif not msg.reply_to_message and (not args or (
+            len(args) >= 1 and not args[0].startswith("@") and not args[0].isdigit() and not msg.parse_entities(
+        [MessageEntity.TEXT_MENTION]))):
+        msg.reply_text("I can't extract a user from this.")
+        return
+
+    else:
+        return
+
+    text = "<b>Zanyarîya Bikarhêner</b>:" \
+           "\nID: <code>{}</code>" \
+           "\n Navê yekem: {}".format(user.id, html.escape(user.first_name))
+
+    if user.last_name:
+        text += "\nNavê duyem: {}".format(html.escape(user.last_name))
+
+    if user.username:
+        text += "\nUsername: @{}".format(html.escape(user.username))
+
+    text += "\nLînk: {}".format(mention_html(user.id, "lînk"))
+
+    if user.id == OWNER_ID:
+        text += "\n\nThis person is my owner - I would never do anything against them!"
+    else:
+        if user.id in SUDO_USERS:
+            text += "\nThis person is one of my sudo users! " \
+                    "Nearly as powerful as my owner - so watch it."
+        else:
+            if user.id in SUPPORT_USERS:
+                text += "\nThis person is one of my support users! " \
+                        "Not quite a sudo user, but can still gban you off the map."
+
+            if user.id in WHITELIST_USERS:
+                text += "\nThis person has been whitelisted! " \
+                        "That means I'm not allowed to ban/kick them."
+
+    for mod in USER_INFO:
+        mod_info = mod.__user_info__(user.id).strip()
+        if mod_info:
+            text += "\n\n" + mod_info
+
+    update.effective_message.reply_text(text, parse_mode=ParseMode.HTML)
+
+
+
+
+@run_async
+def echo(bot: Bot, update: Update):
+    args = update.effective_message.text.split(None, 1)
+    message = update.effective_message
+    if message.reply_to_message:
+        message.reply_to_message.reply_text(args[1])
+    else:
+        message.reply_text(args[1], quote=False)
+    message.delete()
+
+
+MARKDOWN_HELP = """
+Markdown is a very powerful formatting tool supported by telegram. {} has some enhancements, to make sure that \
+saved messages are correctly parsed, and to allow you to create buttons.
+
+- <code>_italic_</code>: wrapping text with '_' will produce italic text
+- <code>*bold*</code>: wrapping text with '*' will produce bold text
+- <code>`code`</code>: wrapping text with '`' will produce monospaced text, also known as 'code'
+- <code>[sometext](someURL)</code>: this will create a link - the message will just show <code>sometext</code>, \
+and tapping on it will open the page at <code>someURL</code>.
+EG: <code>[test](example.com)</code>
+
+- <code>[buttontext](buttonurl:someURL)</code>: this is a special enhancement to allow users to have telegram \
+buttons in their markdown. <code>buttontext</code> will be what is displayed on the button, and <code>someurl</code> \
+will be the url which is opened.
+EG: <code>[This is a button](buttonurl:example.com)</code>
+
+If you want multiple buttons on the same line, use :same, as such:
+<code>[one](buttonurl://example.com)
+[two](buttonurl://google.com:same)</code>
+This will create two buttons on a single line, instead of one button per line.
+
+Keep in mind that your message <b>MUST</b> contain some text other than just a button!
+""".format(dispatcher.bot.first_name)
+
+
+@run_async
+def markdown_help(bot: Bot, update: Update):
+    update.effective_message.reply_text(MARKDOWN_HELP, parse_mode=ParseMode.HTML)
+    update.effective_message.reply_text("Try forwarding the following message to me, and you'll see!")
+    update.effective_message.reply_text("/save test This is a markdown test. _italics_, *bold*, `code`, "
+                                        "[URL](example.com) [button](buttonurl:github.com) "
+                                        "[button2](buttonurl://google.com:same)")
+
+
+@run_async
+def stats(bot: Bot, update: Update):
+    update.effective_message.reply_text("Current stats:\n" + "\n".join([mod.__stats__() for mod in STATS]))
+
+
+# /ip is for private use
+__help__ = """
+ - /id: get the current group id. If used by replying to a message, gets that user's id.
+ - /runs: reply a random string from an array of replies.
+ - /slap: slap a user, or get slapped if not a reply.
+ - /info: get information about a user.
+ - /gdpr: deletes your information from the bot's database. Private chats only.
+
+ - /markdownhelp: quick summary of how markdown works in telegram - can only be called in private chats.
+"""
+
+__mod_name__ = "Misc"
+
+ID_HANDLER = DisableAbleCommandHandler("id", get_id, pass_args=True)
+IP_HANDLER = CommandHandler("ip", get_bot_ip, filters=Filters.chat(OWNER_ID))
+
+INFO_HANDLER = DisableAbleCommandHandler("info", info, pass_args=True)
+
+ECHO_HANDLER = CommandHandler("echo", echo, filters=Filters.user(OWNER_ID))
+MD_HELP_HANDLER = CommandHandler("markdownhelp", markdown_help, filters=Filters.private)
+
+STATS_HANDLER = CommandHandler("stats", stats, filters=CustomFilters.sudo_filter)
+
+dispatcher.add_handler(ID_HANDLER)
+dispatcher.add_handler(IP_HANDLER)
+# dispatcher.add_handler(TIME_HANDLER)
+dispatcher.add_handler(INFO_HANDLER)
+dispatcher.add_handler(ECHO_HANDLER)
+dispatcher.add_handler(MD_HELP_HANDLER)
+dispatcher.add_handler(STATS_HANDLER)
